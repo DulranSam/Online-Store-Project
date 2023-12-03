5c855b1c9d584a6a45c91e1352b06dd42c5d86bb
Dulran Sam <127298642+DulranSam@users.noreply.github.com>
2023-12-03 17:18:13 +0530

    Update storeActions.js

--- backend/routes/storeActions.js
+++ backend/routes/storeActions.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const dataController = require("../controllers/dataController");
 const storeModel = require("../models/store");
 
-router.route("/").get(dataController.getItems).delete()
+router.route("/").get(dataController.getItems).delete(dataController.deleteItems).post(dataController.createItem).put(dataController.updateItems); //I created the routes previously , somehow they got deleted? 
+
 
 
 module.exports = router;
